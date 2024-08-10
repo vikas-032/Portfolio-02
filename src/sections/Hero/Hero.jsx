0ea1f1ef31af1cc45b2f37ac1aadbbdb18e28359
Vikas <7014.vikas@gmail.com>
2024-08-10 12:39:09 +0530

    Day02 adding icons in Portfolio

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,12 +1,13 @@
 import styles from "./HeroStyles.module.css";
 import heroImg from "../../assets/hero-img.png";
-import themeIcon from "../../assets/sun.svg"
-
-
+import themeIcon from "../../assets/sun.svg";
+import twitterIcon from "../../assets/twitter-light.svg";
+import githubIcon from "../../assets/github-light.svg";
+import linkdinIcon from "../../assets/linkedin-light.svg";
 
 function Hero() {
   return (
-    <section id="hero">
+    <section id="hero" className={styles.container}>
       <div className={styles.colorModelContainer}>
         <img
           className={styles.hero}
@@ -14,10 +15,34 @@ function Hero() {
           alt="Profile picture of Vikas Kumar"
         />
 
-        <img  className={styles.colorMode} 
-        src={themeIcon} 
-        alt="Color mode icon" 
+        <img
+          className={styles.colorMode}
+          src={themeIcon}
+          alt="Color mode icon"
         />
+      </div>
+
+      <div className={styles.info}>
+        <h1>
+          Vikas
+          <br />
+          Kumar
+        </h1>
+        <h2>Frontend Developer</h2>
+
+        <span>
+          <a href="https://twitter.com" target="_blank">
+            <img src={twitterIcon} alt="Twitter icon" />
+          </a>
+
+          <a href="https://github.com" target="_blank">
+            <img src={githubIcon} alt="Github icon" />
+          </a>
+
+          <a href="https://linkdin.com" target="_blank">
+            <img src={linkdinIcon} alt="linkdin icon" />
+          </a>
+        </span>
       </div>
     </section>
   );
